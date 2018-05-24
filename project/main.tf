4ab83860c99360debada6d743af525bfd8f56ed9
mateescu <mateescu@ca.ibm.com>
2018-05-24 17:09:52 -0400

    module source

--- project/main.tf
+++ project/main.tf
@@ -3,7 +3,7 @@ provider "google" {
 }
 
 module "tangerine_project" {
-  source = "git::https://github.com/rbmateescu?ref=1.0//google-templates/modules/project"
+  source = "git::https://github.com/rbmateescu/google-templates?ref=1.0//modules/project"
   project_name = "${var.project_name}"
   project_id = "${var.project_id}"
   org_id = "${var.org_id}"
