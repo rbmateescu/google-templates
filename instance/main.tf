b22a27407ac0eaf836131448d267743c08d468bc
mateescu <mateescu@ca.ibm.com>
2018-05-25 11:18:20 -0400

    ref=1.1

--- instance/main.tf
+++ instance/main.tf
@@ -3,7 +3,7 @@ provider "google" {
 }
 
 module "tangerine_instance" {
-  source = "git::https://github.com/rbmateescu/google-templates?ref=1.0//modules/instance"
+  source = "git::https://github.com/rbmateescu/google-templates?ref=1.1//modules/instance"
   unique_resource_name = "${var.unique_resource_name}"
   machine_type = "${var.machine_type}"
   boot_disk = "${var.boot_disk}"
