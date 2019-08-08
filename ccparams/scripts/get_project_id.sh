1b00b8a0e536337aa518f50a85b607318bd1658a
mateescu <mateescu@ca.ibm.com>
2019-08-08 16:53:46 -0400

    gcloud

--- ccparams/scripts/get_project_id.sh
+++ ccparams/scripts/get_project_id.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -x
-PROJECT_ID=`echo $GOOGLE_CREDENTIALS | jq --raw-output '.project_id'`
+#PROJECT_ID=`echo $GOOGLE_CREDENTIALS | jq --raw-output '.project_id'`
+PROJECT_ID=`gcloud version | sed -n 2p | awk '{print $1;}'`
 #PROJECT_ID=`gcloud compute regions list | sed -n 2p | awk '{print $1;}'`
 #echo $PROJECT_ID
 jq -n --arg project_id "$PROJECT_ID" '{"project_id":$project_id}'
