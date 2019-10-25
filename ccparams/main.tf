323c6238c4e5c253b354789da2e558c82ea923aa
mateescu <mateescu@ca.ibm.com>
2019-10-25 07:25:34 -0400

    external

--- ccparams/main.tf
+++ ccparams/main.tf
@@ -2,9 +2,9 @@
 provider "google" {
 }
 
-#data "external" "project_id" {
-#  program = ["/bin/bash", "${path.module}/scripts/get_project_id.sh"]
-#}
+data "external" "project_id" {
+  program = ["/bin/bash", "${path.module}/scripts/get_project_id.sh"]
+}
 
 resource "null_resource" "create-endpoint" {
   provisioner "local-exec" {
