21e9a5b7a59a36bd8dfa179aa35d85a780555782
mateescu <mateescu@ca.ibm.com>
2019-10-25 07:24:00 -0400

    aws

--- ccparams/main.tf
+++ ccparams/main.tf
@@ -2,6 +2,12 @@
 provider "google" {
 }
 
-data "external" "project_id" {
-  program = ["/bin/bash", "${path.module}/scripts/get_project_id.sh"]
+#data "external" "project_id" {
+#  program = ["/bin/bash", "${path.module}/scripts/get_project_id.sh"]
+#}
+
+resource "null_resource" "create-endpoint" {
+  provisioner "local-exec" {
+    command = "/home/terraform/.local/lib/aws/bin/aws --version "
+  }
 }
