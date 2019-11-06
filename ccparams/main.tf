8ec83489d09045b3a4e07a6365090befd4041679
mateescu <mateescu@ca.ibm.com>
2019-11-06 17:06:50 -0500

    read env variables

--- ccparams/main.tf
+++ ccparams/main.tf
@@ -1,13 +1,13 @@
 
-provider "google" {
-}
-
-#data "external" "project_id" {
-#  program = ["/bin/bash", "${path.module}/scripts/get_project_id.sh"]
+#provider "google" {
 #}
 
-resource "null_resource" "create-endpoint" {
-  provisioner "local-exec" {
-    command = "aws --version "
-  }
+data "external" "project_id" {
+  program = ["/bin/bash", "${path.module}/scripts/get_project_id.sh"]
 }
+
+#resource "null_resource" "create-endpoint" {
+#  provisioner "local-exec" {
+#    command = "aws --version "
+#  }
+#}
